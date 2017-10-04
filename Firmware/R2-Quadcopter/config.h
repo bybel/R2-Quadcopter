38b15a76e5db8fd3c763c7e0a45f483de69c0a60
Raphel Fluckiger <raphael.fluckiger@gmail.com>
2017-10-04 15:54:35 +0200

    supression de variables inutiles

--- Firmware/R2-Quadcopter/config.h
+++ Firmware/R2-Quadcopter/config.h
@@ -18,36 +18,28 @@
 #define ROLL_PID_KP  1
 #define ROLL_PID_KI  0.03 
 #define ROLL_PID_KD  1
-#define ROLL_PID_MIN  -200.0
-#define ROLL_PID_MAX  200.0
+#define ROLL_PID_MIN  -400.0
+#define ROLL_PID_MAX  400.0
 
 #define PITCH_PID_KP  1
 #define PITCH_PID_KI  0.03             ////toutes les K a verifier et tester
 #define PITCH_PID_KD  1
-#define PITCH_PID_MIN  -200.0
-#define PITCH_PID_MAX  200.0
+#define PITCH_PID_MIN  -400.0
+#define PITCH_PID_MAX  400.0
 
 #define YAW_PID_KP  1
 #define YAW_PID_KI  0.03
 #define YAW_PID_KD  0
-#define YAW_PID_MIN  -100.0
-#define YAW_PID_MAX  100.0
+#define YAW_PID_MIN  -400.0
+#define YAW_PID_MAX  400.0
 
-#define ROLL_WMIN  -30//////////
-#define ROLL_WMAX  30//////////
-
-#define PITCH_WMIN  -30//////////    valeurs a mapper pour le setpoint
-#define PITCH_WMAX  30//////////
-
-#define YAW_WMIN  -30//////////
-#define YAW_WMAX  30//////////
-
-#define AS_K 1 //auto_adjust --> coef de 1
 
 ////////Config RX///////////////////
-#define THROTTLE_RMIN  1100
+#define THROTTLE_RMIN  1000
+#define THROTTLE_WMIN  1100
 #define THROTTLE_SAFE_SHUTOFF 1120
-#define THROTTLE_RMAX  1850
+#define THROTTLE_RMAX  2000
+#define THROTTLE_WMAX  1850
 #define THROTTLE_RMID  1500
 
 #define ROLL_RMIN  THROTTLE_RMIN
