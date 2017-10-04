f0a38af9c83e92495648b53ab664762ac3a21581
Raphel Fluckiger <raphael.fluckiger@gmail.com>
2017-10-04 19:07:42 +0200

    pid gains pour le level

--- Firmware/R2-Quadcopter/config.h
+++ Firmware/R2-Quadcopter/config.h
@@ -15,24 +15,32 @@
 ////////////////////////////////Definition des constantes///////////////////////////////////////
 
 /////////////PID////////////////
-#define ROLL_PID_KP  1
-#define ROLL_PID_KI  0.03 
-#define ROLL_PID_KD  1
-#define ROLL_PID_MIN  -400.0
-#define ROLL_PID_MAX  400.0
+#define ROLL_PID_KP  0.4
+#define ROLL_PID_KI  0.2 
+#define ROLL_PID_KD  2.2
+#define ROLL_PID_MIN  -200.0
+#define ROLL_PID_MAX  200.0
 
-#define PITCH_PID_KP  1
-#define PITCH_PID_KI  0.03             ////toutes les K a verifier et tester
-#define PITCH_PID_KD  1
-#define PITCH_PID_MIN  -400.0
-#define PITCH_PID_MAX  400.0
+#define PITCH_PID_KP  0.4
+#define PITCH_PID_KI  0.01             ////toutes les K a verifier et tester
+#define PITCH_PID_KD  2.2
+#define PITCH_PID_MIN  -200.0
+#define PITCH_PID_MAX  200.0
 
 #define YAW_PID_KP  1
 #define YAW_PID_KI  0.03
 #define YAW_PID_KD  0
-#define YAW_PID_MIN  -400.0
-#define YAW_PID_MAX  400.0
+#define YAW_PID_MIN  -200.0
+#define YAW_PID_MAX  200.0
 
+////////////////////////LEVEL PIDS
+#define ROLL_PID_LEVEL_KP  0.4
+#define ROLL_PID_LEVEL_KI  0.01
+#define ROLL_PID_LEVEL_KD  2.2
+
+#define PITCH_PID_LEVEL_KP  0.4
+#define PITCH_PID_LEVEL_KI  0.01             ////toutes les K a verifier et tester
+#define PITCH_PID_LEVEL_KD  2.2
 
 ////////Config RX///////////////////
 #define THROTTLE_RMIN  1000
